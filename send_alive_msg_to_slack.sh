e94bb04b2191f6d6098b61e4f59c705ffcaf48c8
sigongweb <kisssulran@i-screammedia.com>
2020-06-25 13:57:06 +0900

    update

--- send_alive_msg_to_slack.sh
+++ send_alive_msg_to_slack.sh
@@ -8,7 +8,7 @@ WEBHOOK_ADDRESS='https://hooks.slack.com/services/TMNFQP8N6/B015A9RN4D9/sNhrX923
 DATE=$(date '+%Y-%m-%d %H:%M:%S')
 
 # json 형식의 ALERT 메시지
-ALERT_TEXT="{\"text\": \"MSG: I'm Alive. Date:  $DATE\"}"
+ALERT_TEXT="{\"text\": \"MSG: Start Container Date:  $DATE\"}"
 
 # alive msg 실행
 curl -X POST -H 'Content-type: application/json' --data "$ALERT_TEXT" $WEBHOOK_ADDRESS > /dev/null 2>&1
