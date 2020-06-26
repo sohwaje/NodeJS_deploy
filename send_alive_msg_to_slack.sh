71ceac5954a9b752bc98c7258dd30db7c50af4a8
sohwaje <kissenyo@gmail.com>
2020-06-26 13:42:09 +0900

    update

--- send_alive_msg_to_slack.sh
+++ send_alive_msg_to_slack.sh
@@ -2,7 +2,7 @@
 # 컨테이너가 실행 -> 앱 URL 체크 -> 성공 또는 실패 메시지를 slack 채널로 보낸다.
 
 # Slack 웹 훅주소
-WEBHOOK_ADDRESS='https://hooks.slack.com/services/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'
+WEBHOOK_ADDRESS='https://hooks.slack.com/services/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'
 # 날짜
 DATE=$(date '+%Y-%m-%d %H:%M:%S')
 # json 형식의 ALERT 메시지
