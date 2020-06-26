e8b1ffc337cef838bcaf8a9cdcced3648e25f0b6
sigongweb <kisssulran@i-screammedia.com>
2020-06-26 10:16:34 +0900

    update

--- send_alive_msg_to_slack.sh
+++ send_alive_msg_to_slack.sh
@@ -6,8 +6,8 @@ WEBHOOK_ADDRESS='https://hooks.slack.com/services/TS56KT49Z/B015U1J13ML/CTNOZpL5
 # 날짜
 DATE=$(date '+%Y-%m-%d %H:%M:%S')
 # json 형식의 ALERT 메시지
-URL_STATUS_OK="{\"text\": \"SUCCESS: Container is running. Date:  $DATE\"}"
-URL_STATUS_Error="{\"text\": \"ERROR: no response Date:  $DATE\"}"
+URL_STATUS_OK="{\"text\": \"SUCCESS: Container is running:$DATE\"}"
+URL_STATUS_Error="{\"text\": \"ERROR: no response:$DATE\"}"
 
 # URL
 URL="http://127.0.0.1:3000"
@@ -15,7 +15,7 @@ URL="http://127.0.0.1:3000"
 # URL 체크
 function check {
   if [ $? == 0 ]; then
-    curl -X POST -H 'Content-type: application/json' --data "$CURL_TEXT" $WEBHOOK_ADDRESS > /dev/null 2>&1
+    curl -X POST -H 'Content-type: application/json' --data "$URL_STATUS_OK" $WEBHOOK_ADDRESS > /dev/null 2>&1
   elif [ $? -ne 0 ]; then
     curl -X POST -H 'Content-type: application/json' --data "$URL_STATUS_Error" $WEBHOOK_ADDRESS > /dev/null 2>&1
   elif [ $? -eq 6 ]; then
