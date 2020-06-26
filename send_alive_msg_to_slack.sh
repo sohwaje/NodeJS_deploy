501329a62ac726d7b9e67338dc2cc44bc2e863ee
sigongweb <kisssulran@i-screammedia.com>
2020-06-26 09:49:54 +0900

    update

--- send_alive_msg_to_slack.sh
+++ send_alive_msg_to_slack.sh
@@ -3,12 +3,28 @@
 
 # Slack 주소
 WEBHOOK_ADDRESS='https://hooks.slack.com/services/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'
-
 # 날짜
 DATE=$(date '+%Y-%m-%d %H:%M:%S')
-
 # json 형식의 ALERT 메시지
-ALERT_TEXT="{\"text\": \"MSG: Start Container Date:  $DATE\"}"
+START_CONTAINER="{\"text\": \"SUCCESS: Start Container. Date:  $DATE\"}"
+URL_STATUS_OK="{\"text\": \"SUCCESS: Container is running. Date:  $DATE\"}"
+URL_STATUS_Error="{\"text\": \"ERROR: no response Date:  $DATE\"}"
 
-# alive msg 실행
-curl -X POST -H 'Content-type: application/json' --data "$ALERT_TEXT" $WEBHOOK_ADDRESS > /dev/null 2>&1
+# URL 체크
+function check {
+  if [ $? == 0 ]; then
+    curl -X POST -H 'Content-type: application/json' --data "$CURL_TEXT" $WEBHOOK_ADDRESS > /dev/null 2>&1
+  elif [ $? -ne 0 ]; then
+    curl -X POST -H 'Content-type: application/json' --data "$URL_STATUS_Error" $WEBHOOK_ADDRESS > /dev/null 2>&1
+  elif [ $? -eq 6 ]; then
+    curl -X POST -H 'Content-type: application/json' --data "$URL_STATUS_Error" $WEBHOOK_ADDRESS > /dev/null 2>&1
+  elif [ $? -eq 7 ]; then
+    curl -X POST -H 'Content-type: application/json' --data "$URL_STATUS_Error" $WEBHOOK_ADDRESS > /dev/null 2>&1
+  else
+    curl -X POST -H 'Content-type: application/json' --data "$URL_STATUS_Error" $WEBHOOK_ADDRESS > /dev/null 2>&1
+  fi
+  exit 1
+}
+
+# check 함수 실행
+check
