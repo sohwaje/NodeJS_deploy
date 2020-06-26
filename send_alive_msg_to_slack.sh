31415e0f17c8e98ae7d17274d427e23ba352cd79
sigongweb <kisssulran@i-screammedia.com>
2020-06-26 10:12:15 +0900

    update

--- send_alive_msg_to_slack.sh
+++ send_alive_msg_to_slack.sh
@@ -6,7 +6,6 @@ WEBHOOK_ADDRESS='https://hooks.slack.com/services/TS56KT49Z/B015U1J13ML/CTNOZpL5
 # 날짜
 DATE=$(date '+%Y-%m-%d %H:%M:%S')
 # json 형식의 ALERT 메시지
-START_CONTAINER="{\"text\": \"SUCCESS: Start Container. Date:  $DATE\"}"
 URL_STATUS_OK="{\"text\": \"SUCCESS: Container is running. Date:  $DATE\"}"
 URL_STATUS_Error="{\"text\": \"ERROR: no response Date:  $DATE\"}"
 
