f3a1a91e87c0e0e371d82b748e3f3ca282cc630a
sohwaje <kissenyo@gmail.com>
2020-06-29 15:21:50 +0900

    Update send_alive_msg_to_slack.sh

--- send_alive_msg_to_slack.sh
+++ send_alive_msg_to_slack.sh
@@ -1,29 +1,33 @@
 #!/bin/sh
 ########### #################################################################
-# 컨테이너 실행 -> 앱 URL 체크 -> 성공 또는 실패 메시지를 slack 채널로 보낸다. #
+# 컨테이너가 실행 -> 앱 URL 체크 -> 성공 또는 실패 메시지를 slack 채널로 보낸다. #
 #############################################################################
 
 # 상태체크해야 할 컨테이너의 URL
 URL="http://127.0.0.1:3000"
 
 # Slack 웹 훅주소
-WEBHOOK_ADDRESS='http://'
+WEBHOOK_ADDRESS='https://hooks.slack.com/services/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'
 
-# 슬랙으로 메시지 보내기 함수 good = 녹색, false = 빨간색
+# 슬랙으로 메시지 보내기 함수
 slack_message(){
     # $1 : message
     # $2 : true=good, false=danger
 
-  COLOR="danger"
-  if $2 ; then
-      COLOR="good"
-  fi
-  curl -s -d 'payload={
+    if [ $2 = false ] ; then
+        COLOR="danger"
+        icon_emoji=":scream:"
+    else
+        COLOR="good"
+        icon_emoji=":smile:"
+    fi
+    curl -s -d 'payload={
       "attachments":
       [
         {"color":"'"$COLOR"'",
+         "title":"Corp : i-SCREAMedia",
          "pretext":"<!channel> *NODEJS DEPLOY INFORMATION*",
-         "text":"*HOST* : '"$HOSTNAME"' \n*MESSAGE* : '"$1"'"}
+         "text":"*HOST* : '"$HOSTNAME"'\n*MESSAGE* : '"$1"' '"$icon_emoji"'"}
       ]
   }' $WEBHOOK_ADDRESS > /dev/null 2>&1
 }
@@ -31,7 +35,7 @@ slack_message(){
 # 생성된 컨테이너의 URL을 체크
 function check_url() {
   curl -s -o "/dev/null" $URL
-  if [ $? = 0 ]; then # Bash에서는 "=="가 맞지만, Bourne shell에서는 "="를 쓴다.
+  if [ $? = 0 ]; then
     slack_message "container is running " true  # 성공
     exit 0
   else
@@ -40,5 +44,4 @@ function check_url() {
   fi
 }
 
-# URL 체크 실행
 check_url
