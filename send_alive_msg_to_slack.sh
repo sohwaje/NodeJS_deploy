799863db66f064b67b8a16453087d7f638685bed
sohwaje <kissenyo@gmail.com>
2020-06-30 14:00:41 +0900

    Update send_alive_msg_to_slack.sh

--- send_alive_msg_to_slack.sh
+++ send_alive_msg_to_slack.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 ########### #################################################################
-# 컨테이너가 실행 -> 앱 URL 체크 -> 성공 또는 실패 메시지를 slack 채널로 보낸다. #
+# 컨테이너 실행 -> 앱 URL 체크 -> 성공 또는 실패 메시지를 slack 채널로 보낸다. #
 #############################################################################
 
-# 상태체크해야 할 컨테이너의 URL
+# health check URL과 문자열
+STRINGS="Hello Yusung?"
 URL="http://127.0.0.1:3000"
 
 # Slack 웹 훅주소
@@ -32,14 +33,33 @@ slack_message(){
   }' $WEBHOOK_ADDRESS > /dev/null 2>&1
 }
 
-# 생성된 컨테이너의 URL을 체크
-function check_url() {
-  curl -s -o "/dev/null" $URL
-  if [ $? = 0 ]; then
+# 임시파일 생성 함수
+CREATE_TMPFILE(){
+  mktemp /tmp/tmp.XXXXXX
+}
+
+# health check를 위한 curl 명령어 함수
+DOWNLOAD_URL(){
+  curl -L ${URL} -o ${TMPFILE} 2>/dev/null
+}
+
+# curl에서 문자열 추출
+Match(){
+  grep "${STRINGS}" "${TMPFILE}"
+}
+
+# 문자열 비교 후 슬랙으로 메시지 보내는 함수
+check_url(){
+  TMPFILE=$(CREATE_TMPFILE)
+  DOWNLOAD_URL
+  MATCH_CHRACTER=$(Match)
+  if [ "$MATCH_CHRACTER" = "$STRINGS" ]; then
     slack_message "container is running " true  # 성공
+    rm -f "${TMPFILE}"
     exit 0
   else
     slack_message "failure" false               # 실패
+    rm -f "${TMPFILE}"
     exit 1
   fi
 }
